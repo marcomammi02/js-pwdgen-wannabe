b89d0d0ebab93dedf0a521b2372d16227bb51beb
marcomammi02 <marcomammi2002@gmail.com>
2022-10-19 14:45:29 +0200

    Last commit

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hi... I will create your own INSICURE password with some questions...")
+let name = prompt("What's your name?")
+let surname = prompt("What's your surname?")
+let color = prompt("What's your favourite color?")
+let year = 22
+
+let generatedPassword = 
+
+document.getElementById("password").innerHTML = name + surname + color + year
